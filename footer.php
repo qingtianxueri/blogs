1650850a5cdc7ec0674de3bb192e421a9fcad5db
chenpeiqing1231 <cpeiqing@astreetpress.com>
2017-07-20 15:19:36 +0800

    change the subtheme for curent theme

--- footer.php
+++ footer.php
@@ -2,33 +2,27 @@
 /**
  * The template for displaying the footer.
  *
- * Contains the closing of the #content div and all content after.
+ * Contains the closing of the #content div and all content after
  *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package Astrid
+ * @package Sydney
  */
-
 ?>
-
+			</div>
 		</div>
 	</div><!-- #content -->
 
-	<div class="footer-wrapper">
-		<?php if ( is_active_sidebar( 'footer-1' ) ) : ?>
-			<?php get_sidebar('footer'); ?>
-		<?php endif; ?>
-		
-		<?php $toggle_contact = get_theme_mod('toggle_contact_footer', 1); ?>
-		<?php if ( $toggle_contact ) : ?>
-		<div class="footer-info">
-			<div class="container">
-				<?php astrid_footer_branding(); ?>
-				<?php astrid_footer_contact(); ?>
-			</div>
-		</div>
-		<?php endif; ?>
-	</div>
+	<?php do_action('sydney_before_footer'); ?>
+
+	<?php if ( is_active_sidebar( 'footer-1' ) ) : ?>
+		<?php get_sidebar('footer'); ?>
+	<?php endif; ?>
+
+    <a class="go-top"><i class="fa fa-angle-up"></i></a>
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+	</footer><!-- #colophon -->
+
+	<?php do_action('sydney_after_footer'); ?>
 
 </div><!-- #page -->
 
--- functions.php
+++ functions.php
@@ -9,13 +9,14 @@
 /*
  *to extend the ad theme
  */
-add_action( 'wp_enqueue_scripts', 'child_enqueue_styles',99);
+add_action('wp_enqueue_scripts', 'child_enqueue_styles', 99);
+
 function child_enqueue_styles() {
-  $parent_style = 'parent-style';
-  //To remove the parent style css
-  // wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-  wp_enqueue_style( 'child-style',get_stylesheet_directory_uri() . '/custom.css', array( $parent_style ));
+  wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/css/custom.css');
+
+  wp_dequeue_style('sydney-fonts');
 }
+
 if (get_stylesheet() !== get_template() ) {
   add_filter( 'pre_update_option_theme_mods_' . get_stylesheet(), function ( $value, $old_value ) {
     update_option( 'theme_mods_' . get_template(), $value );
@@ -25,36 +26,3 @@ if (get_stylesheet() !== get_template() ) {
     return get_option( 'theme_mods_' . get_template(), $default );
   } );
 }
-
-/**
- * Footer contact
- */
-if ( ! function_exists( 'astrid_footer_contact' ) ) :
-function astrid_footer_contact() {
-  $footer_contact_address = get_theme_mod('footer_contact_address');
-  $footer_contact_email   = antispambot(get_theme_mod('footer_contact_email'));
-  $footer_contact_phone   = get_theme_mod('footer_contact_phone');
-
-  echo '<div class="footer-contact">';
-  if ($footer_contact_address) {
-    echo '<div class="footer-contact-block">';
-    echo  '<i class="fa fa-home"></i>';
-    echo  '<span>' . esc_html($footer_contact_address) . '</span>';
-    echo '</div>';
-  }
-  if ($footer_contact_email) {
-    echo '<div class="footer-contact-block">';
-    echo  '<a href="mailto:' . esc_attr($footer_contact_email) . '"><i class="fa fa-envelope"></i></a>';
-    echo  '<span><a href="mailto:' . esc_attr($footer_contact_email) . '">' . esc_html($footer_contact_email) . '</a></span>';
-    echo '</div>';
-  }
-  if ($footer_contact_phone) {
-    echo '<div class="footer-contact-block">';
-    echo  '<i class="fa fa-phone"></i>';
-    echo  '<span>' . esc_html($footer_contact_phone) . '</span>';
-    echo '</div>';
-  } 
-  echo '</div>';
-
-}
-endif;
